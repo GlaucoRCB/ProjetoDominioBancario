f48d8e4d37102f3f5372859e30fd54536b5db0a2
glauco <glauquinho2@gmail.com>
2024-05-27 14:33:36 -0300

    segundo commit

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        Cliente glauco = new Cliente();
-        glauco.setNome(("glauco"));
+        Cliente fulano = new Cliente();
+        Cliente ciclano = new Cliente();
+        fulano.setNome(("fulano"));
+        ciclano.setNome(("ciclano"));
 
-        Conta cc = new ContaCorrente(glauco);
-        Conta poupanca = new ContaPoupanca(glauco);
+        Conta cc = new ContaCorrente(fulano);
+        Conta poupanca = new ContaPoupanca(ciclano);
 
         cc.depositar(100);
         cc.transferir(100, poupanca);
+        poupanca.sacar(50);
 
         cc.imprimirExtrato();
         poupanca.imprimirExtrato();
